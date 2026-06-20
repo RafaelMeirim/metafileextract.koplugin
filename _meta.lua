3091073d5570b3cfd84b8b36c3a3c4725747e9a0
Rafael Meirim <meirim.rafael@gmail.com>
2026-06-20 11:08:14 -0300

    Bump version from 1.5.4 to 1.5.5

--- _meta.lua
+++ _meta.lua
@@ -2,7 +2,7 @@ return {
     name        = "MetaFileExtract",
     fullname    = "Meta File Extract",
     description = "KOReader plugin to automatically parse metadata from filenames and sync local .meta description files.",
-    version     = "1.5.4",
+    version     = "1.5.5",
     author      = "Rafael Meirim",
     url         = "https://github.com/rafaelmeirim/metafileextract.koplugin",
     license     = "MIT",
